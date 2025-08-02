492aee949faaa9c90e87605520c554529ede85e6
Anthony Chiluiza <anthony.chiluiza@epn.edu.ec>
2025-08-02 17:02:19 -0500

    Taller de librerias y submodulos

---
+++ librerias-usadas/index.js
@@ -0,0 +1,8 @@
+const paqueteSuma = require('2025a-swgr2-afcc-suma');
+const paqueteResta = require('2025a-swgr2-afcc-resta');
+
+const respuestaSuma = paqueteSuma.suma(1, 2);
+const respuestaResta = paqueteResta.resta(5, 3);
+
+console.log('La respuesta es de la suma es: ', respuestaSuma);
+console.log('La respuesta es de la resta es: ', respuestaResta);
